155d7f01394392f1b3383c767c6ed485f6273e82
Atron91 <pruizmarin2@gmail.com>
2022-09-02 20:33:37 +0200

    commit: Lifecycle 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Pagina1Component } from './pages/pagina1/pagina1.component';
+import { MuestraNombreComponent } from './components/muestra-nombre/muestra-nombre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Pagina1Component
+    Pagina1Component,
+    MuestraNombreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
